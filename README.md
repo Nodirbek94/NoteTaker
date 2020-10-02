b979e9ac3d710786854bda5fb2ea2b8e98deb4f7
Nodirbek94 <nodirbekmaksudov@yahoo.com>
2020-10-02 09:13:50 -0400

    server codes

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# NoteTaker
+# Note Taker ![License](https://img.shields.io/static/v1?label=MIT&message=license&color=red)
+
+## Description
+This application can be used to write, save and delate notes. Front end of the application was prebuild and we had to crete the server side of this application
+
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributions](#contributions)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+The application will be deployed to Heroku and anyon with the deployed link can use the application
+
+
+## Usage
+It can be used to create notes and save them or you can delate the old notes
+
+
+## License
+Notice: This is licensed under MIT.
+![License](https://img.shields.io/static/v1?label=MIT&message=license&color=red)
+
+
+## Contributions
+Anyone can contribute by just forking this repository
+
+
+## Tests
+Application was tested using NPM and was run through the local host
+
+
+## Questions
+Questions about this repository? Please contact me at [nodirbekmaksudov@gmail.com](mailto:nodirbekmaksudov@gmail.com). View more of my work in GitHub at [undefined](https://github.com/undefined) 
+
---
+++ server.js
@@ -0,0 +1,75 @@
+// Dependencies
+// ====================================================
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Sets up the express App
+// =====================================================
+const app = express();
+const port = process.eventNames.PORT || 8080;
+const mainDir = path.join(__dirname, "/public");
+
+// Sets up the Express App to handle data parsing
+// ======================================================
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Routes
+// ======================================================
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+// Saves the new notes
+// =======================================================================
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+})
+
+// Delates the old notes
+// ==========================================================================
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+// Starts the server to begin lostening
+// ==================================================================
+app.listen(port, function() {
+    console.log(`Now listening to port ${port}. Enjoy your stay!`);
+});
